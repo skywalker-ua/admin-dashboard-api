1d6a42747176b2e0b15ff5e5099a0ace17f02ac6
Назар Кривоніс <nazarkrivonis@gmail.com>
2020-05-14 20:48:52 +0300

    burger icon context connect

--- util/database.js
+++ util/database.js
@@ -1,19 +1,19 @@
 const Sequelize = require('sequelize');
 
 //Production db
-const sequelize = new Sequelize(
-    'mysql://b7e4a873388d76:31943d47@us-cdbr-east-06.cleardb.net/heroku_5c9556ddcb17cc9?reconnect=true'
-)
-
-// 
 // const sequelize = new Sequelize(
-//     'chemiplast_data',
-//     'root',
-//     'nazarino203',
-//     {
-//         'dialect': 'mysql',
-//         'host': 'localhost'
-//     }
-// );
+//     'mysql://b7e4a873388d76:31943d47@us-cdbr-east-06.cleardb.net/heroku_5c9556ddcb17cc9?reconnect=true'
+// )
+
+//Local db 
+const sequelize = new Sequelize(
+    'chemiplast_data',
+    'root',
+    'nazarino203',
+    {
+        'dialect': 'mysql',
+        'host': 'localhost'
+    }
+);
 
 module.exports = sequelize;
