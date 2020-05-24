aac1ead9f344814f0bfa9007264170cd94f29bec
Назар Кривоніс <nazarkrivonis@gmail.com>
2020-05-24 17:20:29 +0300

    cors

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ require('dotenv').config();
 const adminRoutes = require('./routes/admin');
 const port = process.env.PORT || 5000;
 const sequelize = require('./util/database');
-
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors);
 app.use(adminRoutes);
 
 sequelize   
