af65fbace2075c6503167fb797f82a7c78f9d0aa
Назар Кривоніс <nazarkrivonis@gmail.com>
2020-05-25 20:57:34 +0300

    heroku config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const adminRoutes = require('./routes/admin');
 const port = process.env.PORT || 5000;
 const sequelize = require('./util/database');
